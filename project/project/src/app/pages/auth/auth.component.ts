78bac5cd523ae1ada8db74b75d696072a4b030ee
nyllonxdd <dontknowww9142@gmail.com>
2025-10-21 15:23:12 +0530

    Updated auth.component.ts

--- project/project/src/app/pages/auth/auth.component.ts
+++ project/project/src/app/pages/auth/auth.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
+//import { createClient, SupabaseClient } from '@supabase/supabase-js';
 import { environment } from '../../../environments/environment';
 
 @Component({
