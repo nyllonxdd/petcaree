db683a7054fbb5eb74c90fa0e178a64c1b553a54
nyllonxdd <dontknowww9142@gmail.com>
2025-10-21 15:23:31 +0530

    Updated dashboard.component.ts

--- project/project/src/app/pages/dashboard/dashboard.component.ts
+++ project/project/src/app/pages/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
+//import { createClient, SupabaseClient } from '@supabase/supabase-js';
 import { environment } from '../../../environments/environment';
 
 interface Pet {
